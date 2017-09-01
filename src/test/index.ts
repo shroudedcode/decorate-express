266ef408d66b3594ad5599a3e0a5b9f39e9a35c1
Niklas Higi <niklas@shroudedcode.com>
2017-09-01 13:46:44 +0200

    Rewrite unit tests using Mocha and Chai

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,97 +1,122 @@
-import test from 'ava';
-import * as web from '../lib';
-import * as express from 'express';
-import * as supertest from 'supertest-as-promised';
+import * as chai from 'chai'
+const chaiHttp = require('chai-http')
+import { expect } from 'chai'
+import * as web from '../lib'
+import * as express from 'express'
+import * as supertest from 'supertest-as-promised'
 
+chai.use(chaiHttp)
 
-test('get', (t) => {
+describe('The @get decorator', () => {
   class Test {
-    @web.get('/test') getTest() { }
+    @web.get('/test') getTest() {}
   }
 
-  let routes = web.getRoutes(new Test());
-  t.is(routes.length, 1);
+  const routes = web.getRoutes(new Test())
+  const route = routes[0]
 
-  let route = routes[0];
-  t.is(route.key, 'getTest');
-  t.is(route.handlers.length, 1);
-  t.is(route.method, 'get');
-  t.is(route.path, '/test');
-});
+  it('should return the correct amount of routes',
+    () => expect(routes).to.have.lengthOf(1))
+
+  it('should assign the correct key',
+    () => expect(route.key).to.equal('getTest'))
+
+  it('should assign a handler',
+    () => expect(route.handlers).to.have.lengthOf(1))
+
+  it('should assign the correct method',
+    () => expect(route.method.toLowerCase()).to.equal('get'))
+
+  it('should assign the correct path',
+    () => expect(route.path).to.equal('/test'))
+})
 
 
-test('multiple', (t) => {
+describe('Multiple decorated routes', () => {
   class Test {
-    @web.get('/test') getTest() { }
-    @web.post('/test') postTest() { }
+    @web.get('/test') getTest() {}
+    @web.post('/test') postTest() {}
   }
 
-  let routes = web.getRoutes(new Test());
-  t.is(routes.length, 2);
-});
+  const routes = web.getRoutes(new Test())
+  it('should work as well',
+    () => expect(routes).to.have.lengthOf(2))
+})
 
 
-test('basePath', (t) => {
+describe('The @basePath decorator', () => {
   @web.basePath('/test')
   class Test {
-    @web.get('/foo') getTest() { };
+    @web.get('/foo') getTest() {}
   }
 
-  let route = web.getRoutes(new Test())[0];
-  t.is(route.path, '/test/foo');
-});
+  const route = web.getRoutes(new Test())[0]
+  it('should prepend all routes in the decorated class',
+    () => expect(route.path).to.equal('/test/foo'))
+})
 
 
-test('middleware string', (t) => {
+describe('The @middleware decorator', () => {
   @web.basePath('/test')
   class Test {
     @web.middleware('testMiddleware')
-    @web.get('/foo') getTest() { };
+    @web.get('/foo') getTest() {}
 
-    testMiddleware() {
-    }
+    testMiddleware() {}
   }
 
-  let route = web.getRoutes(new Test())[0];
-  t.is(route.path, '/test/foo');
-  t.is(route.handlers.length, 2);
-});
+  const route = web.getRoutes(new Test())[0]
+  it('should assign itself to routes',
+    () => expect(route.handlers).to.have.lengthOf(2))
+  it('should not affect @basePath\'s',
+    () => expect(route.path).to.equal('/test/foo'))
+})
 
 
-test('express', (t) => {
+const classSuite = describe('A decorated class', () => {
   @web.basePath('/test')
   class Test {
-    bar = 'hello';
+    bar = 'hello'
 
     @web.use()
     setup(request, response, next) {
-      request.foo = 8;
-      t.is(this.bar, 'hello');
-      next();
+      request.foo = 8
+      it('should make default class properties accessible in @use middleware',
+        () => expect(this.bar).to.equal('hello'))
+      next()
     }
 
     @web.param('id')
     idParam(request, response, next, id) {
-      request.params.id = parseInt(request.params.id);
-      t.is(this.bar, 'hello');
-      next();
+      request.params.id = parseInt(request.params.id)
+      it('should make default class properties accessible in @param middleware',
+        () => expect(this.bar).to.equal('hello'))
+      next()
     }
 
     @web.get('/foo/:id')
     foo(request, response) {
-      t.is(request.params.id, 5);
-      t.is(request.foo, 8);
-      t.is(this.bar, 'hello');
-      response.send();
+      it('should make use of middleware decorated with @param when needed',
+        () => expect(request.params.id).to.equal(5))
+      it('should make use of middleware decorated with @use',
+        () => expect(request.foo).to.equal(8))
+      it('should make default class properties accessible in @param middleware',
+        () => expect(this.bar).to.equal('hello'))
+      response.send()
     }
   }
 
-  let app = express();
-  let controller = new Test();
-  web.register(app, controller);
-  t.plan(5);
+  const app = express()
+  const controller = new Test()
+  web.register(app, controller)
 
-  return supertest(app)
-    .get('/test/foo/5')
-    .expect(200);
-});
+  it('should work without errors inside an express app', (done) => {
+    chai.request(app)
+      .get('/test/foo/5')
+      .end((err, res) => {
+        expect(err).to.be.null
+        expect(res).to.have.status(200)
+        done()
+      })
+  })
+})
